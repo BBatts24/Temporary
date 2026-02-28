bd16dfd01876a55777770a1aba826f19472a93f1
BBatts24 <philipcolborn@gmail.com>
2026-02-28 12:01:32 -0500

    fix2

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <body style={{ backgroundColor: 'white' }}>
+      <header className="fixed-header" style={{ backgroundColor: 'black', textAlign: 'left', padding: '1rem' }}>
+        <h1 style={{ color: 'white' }}>Phishing Challenge!</h1>
+      </header>
+      <div> 
+      </div>
+    </body>
+  )
+}
+
+export default App
